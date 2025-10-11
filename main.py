171fad0da75e99bf905b0f17fe0b433663bc5596
Louis-Thomas Phaneuf <ltphaneuf9@gmail.com>
2025-10-11 07:50:43 -0400

    Ajout du main en version POO et idée potentiel pour self.stocks

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import customtkinter
+import customtkinter as ctk
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
 
 APP_GEOMETRY = "400x150"
+APP_TITLE = "Paper Trading"
 
-app = customtkinter.CTk()
-app.geometry(APP_GEOMETRY)
-app.mainloop()
+class MainApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry(APP_GEOMETRY)
+        self.title(APP_TITLE)
+        self.show_accueil()
+        self.stocks = {
+            "TSLA" : yf.download("TSLA", start="2024-01-01", end="2025-10-11", interval="1d"),
+            "AAPL" : yf.download("AAPL", start="2024-01-01", end="2025-10-11", interval="1d"),
+            "NVDA" : yf.download("NVDA", start="2024-01-01", end="2025-10-11", interval="1d")
+        }
+    
+if __name__ == "__main__":
+    app = MainApp()
+    app.mainloop()
