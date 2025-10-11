ed052661df04a11643c95d9533d726187a1f8881
Louis-Thomas Phaneuf <ltphaneuf9@gmail.com>
2025-10-11 09:06:22 -0400

    Commencer la watchlist: Titre, titre du stock et prix, maistrès de base. Le code en commentaire ca fait partie du labo ctait juste pour m'orienté.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import customtkinter as ctk
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from watchlist import Watchlist
 
 
-APP_GEOMETRY = "400x150"
+APP_GEOMETRY = "800x600"
 APP_TITLE = "Paper Trading"
 
 class MainApp(ctk.CTk):
@@ -12,13 +13,17 @@ class MainApp(ctk.CTk):
         super().__init__()
         self.geometry(APP_GEOMETRY)
         self.title(APP_TITLE)
-        self.show_accueil()
         self.stocks = {
             "TSLA" : yf.download("TSLA", start="2024-01-01", end="2025-10-11", interval="1d"),
             "AAPL" : yf.download("AAPL", start="2024-01-01", end="2025-10-11", interval="1d"),
             "NVDA" : yf.download("NVDA", start="2024-01-01", end="2025-10-11", interval="1d")
         }
+        print(self.stocks["TSLA"].head())
+        self.show_watchlist()
+
+    def show_watchlist(self):
+        self.watchlist = Watchlist(master=self,stocks=self.stocks)
     
 if __name__ == "__main__":
-    app = MainApp()
+    app = MainApp() 
     app.mainloop()
