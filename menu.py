083d7de52515100a7f26cbbe63ac5edc13990505
Louis-Thomas Phaneuf <ltphaneuf9@gmail.com>
2025-11-14 16:09:55 -0500

    ajout du graph

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import tkinter as tk
 from tkinter import Menu
 from info import Info
+from graphe import Graph
 import pandas as pd
 
 class MenuBar(ctk.CTkFrame):
@@ -24,31 +25,33 @@ class MenuBar(ctk.CTkFrame):
         quitter_menu.add_command(label="Quitter", command=self.quit)
         menu_bar.add_cascade(label="Quitter", menu=quitter_menu)
 
+        # ajout du menu info
         info_menu = Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Informations", menu=info_menu)
 
         for stock in self.stocks:
             info_menu.add_command(label=stock, command=lambda s=stock : self.open_info(s))
 
-        edit_menu = Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Édition", menu=edit_menu)
-        edit_menu.add_command(label="Annuler", command=self.undo_action)
-        edit_menu.add_command(label="Rétablir", command=self.redo_action)
-        view_menu = Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Affichage", menu=view_menu)
-        view_menu.add_command(label="Accueil", command=self.show_accueil)
+        # ajout du menu graph
+        graph_menu = Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Graphe", menu=graph_menu)
+
+        for stock in self.stocks:
+            graph_menu.add_command(label=stock, command=lambda s=stock : self.open_graph(s))
+
+        # view_menu = Menu(menu_bar, tearoff=0)
+        # menu_bar.add_cascade(label="Affichage", menu=view_menu)
+        # view_menu.add_command(label="Accueil", command=self.show_accueil)
 
     def open_info(self, stock):
         self.master.clear_main_frame()
         self.master.update_menu(self.stocks, self.temps, self.compte)
         Info(self.master, self.stocks, stock, self.temps, self.compte)
-    def save_file(self):
-        print("Enregistrer le fichier")
-    def undo_action(self):
-        print("Annuler")
-    def redo_action(self):
-        print("Rétablir")
-    def show_accueil(self):
-        print("Acceuile")
+    
+    def open_graph(self, stock):
+        self.master.clear_main_frame()
+        self.master.update_menu(self.stocks, self.temps, self.compte)
+        Graph(self.master, self.stocks, stock, self.temps, self.compte)
+ 
 
     
